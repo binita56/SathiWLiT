beaede5ea30c68d4d9cc94ac7cf290e0eeb268f7
ranaprabina <ranaprabina953@gmail.com>
2022-03-09 14:28:38 +0545

    addedmodels

--- sathiApp/routes/index.js
+++ sathiApp/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+const { resource } = require('../app');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/result-list', function (req, res, next) {
+  res.render('resultList');
+})
 module.exports = router;
